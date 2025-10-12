aa8402a4a37c748349479c47ba1d503718b15ac3
LH Torquette <lucastorquette@gmail.com>
2025-10-12 11:49:58 -0300

    move .envile to .env.development

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -107,7 +107,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "4db4ba2849d26508"; }
+/******/ 		__webpack_require__.h = function() { return "47150fc85989c6d2"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
