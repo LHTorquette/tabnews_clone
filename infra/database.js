b5182db07c2fe1997d4c5faacd73158125c26ca1
LH Torquette <lucastorquette@gmail.com>
2025-10-31 23:17:01 -0300

    trocando o db para o neon

--- infra/database.js
+++ infra/database.js
@@ -7,9 +7,17 @@ async function query(queryObject) {
     user: process.env.POSTGRES_USER,
     database: process.env.POSTGRES_DB,
     password: process.env.POSTGRES_PASSWORD,
+    ssl: process.env.NODE_ENV === "development" ? false : true,
   });
   try {
     await client.connect();
+    console.log("CREDENTIALS Client:", {
+      host: process.env.POSTGRES_HOST,
+      port: process.env.POSTGRES_PORT,
+      user: process.env.POSTGRES_USER,
+      database: process.env.POSTGRES_DB,
+      password: process.env.POSTGRES_PASSWORD,
+    });
     const result = await client.query(queryObject);
 
     return result;
